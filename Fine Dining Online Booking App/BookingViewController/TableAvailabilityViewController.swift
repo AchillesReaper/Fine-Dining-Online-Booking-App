1a021ad6d0da4eff7c408b310622f0538ebd991b
EricChen12 <cg934121665@gmail.com>
2022-05-13 17:03:51 +1000

    Adding table availability and appointment history view - Eric

---
+++ Fine Dining Online Booking App/BookingViewController/TableAvailabilityViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TableAvailabilityViewController.swift
+//  Fine Dining Online Booking App
+//
+//  Created by Guo Chen on 13/5/2022.
+//
+
+import Foundation
+import UIKit
+
+class TableAvailabilityViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
