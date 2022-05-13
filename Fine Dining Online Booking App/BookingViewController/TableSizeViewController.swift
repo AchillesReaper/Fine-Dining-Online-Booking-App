2eb9fd3e86d04439abd06698f995a6af2e954de3
EricChen12 <cg934121665@gmail.com>
2022-05-13 16:51:36 +1000

    Adding table size view - Eric

---
+++ Fine Dining Online Booking App/BookingViewController/TableSizeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TableSizeViewController.swift
+//  Fine Dining Online Booking App
+//
+//  Created by Guo Chen on 13/5/2022.
+//
+
+import Foundation
+import UIKit
+
+class TableSizeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
